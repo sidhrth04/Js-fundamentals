91ef0140b3f165be58dc8bcd154c5872e3ee9838
Siddharth <92026202+sidhrth04@users.noreply.github.com>
2024-01-01 12:38:46 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "sid@google.com"
+var accountPassword = "12345" //should not be used/prefer not to use
+accountCity = "Jaipur"
+let accountstate; // js gives undefined is no value
+//for variable let and var
+
+/*var has the scope problem({
+
+})
+*/
+
+// accountId = 2 not allowed
+
+accountEmail = "hc@yahoo.com"
+accountPassword = "1223234"
+accountCity = "bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountstate])
