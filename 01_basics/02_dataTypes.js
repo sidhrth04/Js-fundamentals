2dc4be15d8914512fb86d277004514314a7e83cb
Siddharth <92026202+sidhrth04@users.noreply.github.com>
2024-01-02 17:44:11 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treats all JS code as newer version
+
+// alert(3+3) //wont work because we are using nodejs and not browser/document
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "sid"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
