1027b33db05ba6e0991b28bff3ecff4d81f29672
Siddharth <92026202+sidhrth04@users.noreply.github.com>
2024-01-03 15:25:18 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = "33ab"
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+//33 => 33
+//"33abc" => Nan
+//true => 1; false =>0
+
+// let loggin = 1
+// let booleanLogin = Boolean(loggin)
+
+// console.log(booleanLogin);
+
+
+//"" => false
+//"sid" => true
+
+let someNumber  = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
