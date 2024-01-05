2d873db4c4ed2266bc133e65d48dceec8152d350
Siddharth <92026202+sidhrth04@users.noreply.github.com>
2024-01-05 15:17:35 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,5 +23,42 @@
 let someNumber  = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************************************ Operations *********************************
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2); Addition
+// console.log(2-2); Substarction
+// console.log(2*2); Multiplication
+// console.log(2**3); Power
+// console.log(2/3); Division
+// console.log(2%3); Remainder
+
+let str1 = "hello "
+let str2 = "Sid"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1"+2); //output 12
+console.log(1+"2"); //output 12
+console.log("1"+2+2); //output 122 basically jo datatype first hoga usme convert hojayega
+console.log(1+2+"2"); //output 32 yaha first number that toh converted into number
+
+//writing above type of code usually not prefered
+
+// console.log(+true); //output 1
+// console.log(+""); //output 0
+
+let gameCounter = 100
+gameCounter++;
+
+console.log(gameCounter);
