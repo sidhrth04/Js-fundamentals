0a411c77b4139561708e9f9fbedb89cb363fa911
Siddharth <92026202+sidhrth04@users.noreply.github.com>
2024-01-05 21:29:24 +0000

    comaprison

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,7 +1,28 @@
-console.log(2>1);
-console.log(2 >= 1);
-console.log(2 < 1);
-console.log(2 == 1);
-console.log(2 != 1);
+// console.log(2>1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
 
 //problem comes when we have to compare different datatypes
+
+// console.log("2">1); //converts the string into a number
+// console.log("02">1);
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0);//true
+
+//reason being that in js equality and comparison check works differently
+//comparison converts null to a number which is 0 thats the 1st case is false.
+
+//we have to mostly avoid such comparisons for clean code
+
+// console.log(undefined == 0);//false
+// console.log(undefined > 0);//false
+// console.log(undefined < 0);//false
+
+/*Now comes the strict check which is "===" (Triple equal) 
+which not only checks the value but also checks the datatype of the value*/
+
+console.log("2"===2); //checks the datatype as well
