a783dc9827ff587374507b10e66021f83162c261
Siddharth <92026202+sidhrth04@users.noreply.github.com>
2024-01-07 11:11:23 +0000

    typesofmemory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -23,7 +23,7 @@ const anotherId = Symbol("123")
 
 //symbol basically stores things uniquely even tho the values are same
 
-console.log(Id === anotherId); //false
+// console.log(Id === anotherId); //false
 
 //Non Premitive (Reference type)
 
@@ -40,6 +40,42 @@ myObj = {
 }
 
 //Function (type: function object)
-const myFunction = function(){
-    console.log("Hello World");
+// const myFunction = function(){
+//     console.log("Hello World");
+// }
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/*Memory => 2 TYPES => STACK and HEAP 
+ 
+STACK => Used in => Primitive types => Whenever we store something in stack we get the copy of it, Manjhe we do changes in its copy and not on the actual value.
+
+HEAP => Used in => Non-Primitive types => where as when we store something in heap we get its reference or basically the original value and here we do all the changes on the original value.
+
+*/
+//Example of stack memory
+let myName = "PAPA"
+
+let notMyname = myName
+
+notMyname = "BETA"
+
+console.log(notMyname);
+console.log(myName);
+
+
+//Example of heap memory
+
+let userOne = {
+
+    email: "user@gmail.com",
+    upi : "sid@okhdfc"
 }
+
+let userTwo = userOne
+
+userTwo.email = "papa@yahoo.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
