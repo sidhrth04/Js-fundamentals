3921cb9b48555d1a00f8adb7bc1681d62676954f
Siddharth <92026202+sidhrth04@users.noreply.github.com>
2024-01-11 14:28:17 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -38,6 +38,44 @@ function userLogin(username = "sid"){
 //     return `${username} just logged in`
 // }
 
-console.log(userLogin("sam")); // jab koi value he pass nhi ki jaati hai in the place of function  parameter/argument the output comes as UNDEFINED
+// console.log(userLogin("sam")); // jab koi value he pass nhi ki jaati hai in the place of function  parameter/argument the output comes as UNDEFINED
 
 //if we give an argument the default value will be overwritten
+
+//rest operator ...something
+//basically rest operator, when given multiple input returns it as a array
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(2,3,4));
+
+//to pass an object to a function
+
+const user = {
+    username: "siddharth",
+    price: 200
+}
+
+function handleObject(anyobj){
+    console.log(`username is ${anyobj.username} and price is ${anyobj.price}`);
+
+}
+
+//jab function ko call karenge we will pass an object as the argument
+
+// handleObject(user)
+
+// handleObject({ directly object banadiya
+//     username: "sam",
+//     price: 400
+// })
+
+const mynewArr = [200, 400, 600]
+
+
+function returnSecondValue(getarray){
+    return getarray[1]
+}
+console.log(returnSecondValue(mynewArr));
+console.log(returnSecondValue([200,500,700])); //can directly pass variables as well
