8a73c8436d8b119fcb1d06e24b1220a9d198ce17
Siddharth <92026202+sidhrth04@users.noreply.github.com>
2024-01-14 10:21:20 +0000

    cotrol flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -7,4 +7,42 @@ if(isUser){
 //comparison operator 
 // <,>,<=,>=,==,!=,===,!==(checks negative sign),
 
- 
+ // const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
