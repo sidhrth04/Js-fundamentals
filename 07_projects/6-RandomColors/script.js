f5194ba1e9a471097e4fa83b17c64a4b02f1f32d
Siddharth <92026202+sidhrth04@users.noreply.github.com>
2024-03-03 11:52:11 +0000

    Project6

---
+++ 07_projects/6-RandomColors/script.js
@@ -0,0 +1,25 @@
+//generate random colour
+const randomColour = function () {
+    let color = '#';
+    const hex = '0123456789ABCDEF';
+    for (let i = 0; i < 6; i++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+  let intervalId;
+  const startchangeColor = function () {
+    if (!intervalId) {
+      intervalId = setInterval(changeBgColor, 1000);
+    }
+    function changeBgColor() {
+      document.body.style.backgroundColor = randomColour();
+    }
+  };
+  const stopchangeColor = function () {
+    clearInterval(intervalId);
+    intervalId = null;
+  };
+  document.querySelector('#start').addEventListener('click', startchangeColor);
+  document.querySelector('#stop').addEventListener('click', stopchangeColor);
+  
