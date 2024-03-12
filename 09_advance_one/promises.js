bfae36ec19ca281c53fb8c1a35e432275d88918f
Siddharth <92026202+sidhrth04@users.noreply.github.com>
2024-03-12 13:25:20 +0000

    promise

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -85,3 +85,14 @@ consumePromiseFive()
 // }
 
 //getAllUsers()
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+// promise.all
+// yes this is also available, kuch reading aap b kro.
