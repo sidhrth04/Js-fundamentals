66dca0bf3516290e1c6a8fe2b0e4d99b38700578
Siddharth <92026202+sidhrth04@users.noreply.github.com>
2024-03-12 13:24:11 +0000

    promise

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -72,3 +72,16 @@ async function consumePromiseFive(){
     }
 }
 consumePromiseFive()
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+
+//getAllUsers()
