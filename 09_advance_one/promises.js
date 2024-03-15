59015026d907a89f9bd91e3552fcff5c80adff2a
Siddharth <92026202+sidhrth04@users.noreply.github.com>
2024-03-15 23:25:01 +0530

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -5,7 +5,7 @@ const promiseOne = new Promise(function(resolve,reject){
         console.log('Async task is complete');
         resolve()
     },1000)
-})
+}) 
 
 promiseOne.then(function(){
     console.log('Promise Consumed');
